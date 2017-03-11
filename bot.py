bb01abdc8df53c074f5b59f0e4f30d887dd33163
clapmyhands <pineapplepie90@gmail.com>
2017-03-12 01:55:20 +0800

    Initial working bot. can only reply to `~hey`

---
+++ bot.py
@@ -0,0 +1,51 @@
+import claspbot
+from discord.ext import commands
+import discord
+import logging, sys
+import json
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+
+
+
+client = discord.Client()
+prefix = ["~"]
+
+bot = commands.Bot(command_prefix=prefix)
+
+@bot.event
+async def on_ready():
+    logger.info("Logged in as:")
+    logger.info("Username: {}".format(bot.user.name))
+    logger.info("ID: {}".format(bot.user.id))
+
+@bot.command(pass_context=True)
+async def hey(ctx):
+    msg = ctx.message
+    await bot.say("hey {}".format(msg.author.mention))
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+
+
+
+def load_credential():
+    with open('credentials.json','r') as cred:
+        return json.load(cred)
+
+
+def main():
+    cred = load_credential()
+
+    bot.client_id = cred['client_id']
+    token = cred['token']
+
+    bot.run(token)
+
+
+
+if __name__ == '__main__':
+    main()
